9583c2e1ecc37032cf17bc934b2b0e67b2430d9e
thomasalcock <thomas.alcock@statworx.com>
2026-03-21 12:08:03 +0100

    treat warnings as errors

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ MODE=$1
 if [[ $MODE = "debug"  ]]; then
  g++ -g main.cpp tinyfiledialogs/tinyfiledialogs.c -I tinyfiledialogs \
    -o $EXE --std=c++20 \
-   -Wall -Wextra -Wpedantic \
+   -Wall -Wextra -Wpedantic -Werror \
    -lraylib -lm -lpthread -lrt -lX11 \
     && ./$EXE 
 elif [[ $MODE = "release" ]]; then
  g++ main.cpp -o $EXE -O2 --std=c++20 \
-   -Wall -Wextra -Wpedantic \
+   -Wall -Wextra -Wpedantic -Werror \
    -lraylib -lm -lpthread -lrt -lX11 && ./$EXE
 fi
